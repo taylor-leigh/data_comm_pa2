a5d54e09d538415cf55e3def59a1bb9eb7dc3810
taylor <taylor.leigh.morgan97@gmail.com>
2020-10-03 17:50:31 -0500

    framework for udp sockets

--- client.cpp
+++ client.cpp
@@ -1,2 +1,55 @@
 // Taylor Morgan - tm2045
 // Khalil Markham - km1783
+
+#include <iostream>
+#include <sys/types.h>   // defines types (like size_t)
+#include <sys/socket.h>  // defines socket class
+#include <netinet/in.h>  // defines port numbers for (internet) sockets, some address structures, and constants
+#include <netdb.h>
+#include <iostream>
+#include <fstream>
+#include <arpa/inet.h>   // if you want to use inet_addr() function
+#include <string.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+  struct hostent *s;
+  s = gethostbyname(argv[1]);
+
+  struct sockaddr_in server;
+  int mysocket = 0;
+  socklen_t slen = sizeof(server);
+  char payload[512];
+  char ack[512];
+  int port = argv[2];
+
+  if ((mysocket=socket (AF_INET, SOCK_DGRAM, 0))==-1)
+    cout << "Error in creating socket.\n";
+
+  // get name of file to transfer
+  char* filename;
+  filename = argv[3];
+
+  // open file
+  ifstream file_to_send;
+  file_to_send.open(filename);
+
+  memset((char *) &server, 0, sizeof(server));
+  server.sin_family = AF_INET;
+  server.sin_port = htons(port);
+  bcopy((char *)s->h_addr,
+        (char *)&server.sin_addr.s_addr,
+	s->h_length;
+
+  if (sendto(mysocket, payload, 30, 0, (struct sockaddr *)&server, slen)==-1)
+    cout << "Error in sendto function.\n";
+
+  recvfrom(mysocket, ack, 512, 0, (struct sockaddr *)&server, &slen);
+
+  close(mysocket);
+  return 0;
+
+}
--- server.cpp
+++ server.cpp
@@ -1,2 +1,45 @@
 // Taylor Morgan - tm2045
 // Khalil Markham - km1783
+
+
+#include <iostream>
+#include <sys/types.h>   // defines types (like size_t)
+#include <sys/socket.h>  // defines socket class
+#include <netinet/in.h>  // defines port numbers for (internet) sockets, some address structures, and constants
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, *argv[]){
+
+  struct sockaddr_in server;
+  struct sockaddr_in client;
+  int mysocket = 0;
+  socklen_t clen = sizeof(client);
+  char payload[512];
+  char ack[512];
+  int port = argv[1];
+
+  if ((mysocket=socket(AF_INET, SOCK_DGRAM, 0))==-1)
+    cout << "Error in socket creation.\n";
+
+  memset((char *) &server, 0, sizeof(server));
+  server.sin_family = AF_INET;
+  server.sin_port = htons(port);
+  server.sin_addr.s_addr = hton1(INADDR_ANY);
+
+  if (bind(mysocket, (struct sockaddr *)&server, sizeof(server)) == -1)
+    cout << "Error in binding.\n";
+
+  if (recvfrom(mysocket, payload, 512, 0, (struct sockaddr *)&client, &clen)==-1)
+    cout << "Failed to receive.\n";
+
+  if (sendto(mysocket, ack, 512, 0, (struct sockaddr *)&client, clen)==-1)
+    cout << "Error in sendto function.\n";
+
+  close(mysocket);
+  return 0;
+
+}
